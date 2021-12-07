27811c584a3936b17fc997f8cc7f16f868e559ac
mashaly <medo.mashaly.com>
2021-12-07 08:26:53 +0200

    new design

---
+++ food.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+
+ 
+    /*alert($(window).width());*/
+    /*alert($(window).height());*/
+    'use strict';
+    
+    $('.hero').height($(window).height()).width($(window).width());
+
+     
+});
+
